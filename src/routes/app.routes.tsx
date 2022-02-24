7b8f344889b41bb948897dd45b5870b4d36e30f8
Thiago Negreiros <thiago.negreiros@sidia.com>
2022-02-24 15:06:40 -0400

    gofinances: add state bar

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Platform } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons'
+import { useTheme } from "styled-components";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+import { Dashboard } from "../screens/Dashboard"; 
+import { Register } from "../screens/Register"; 
+
+export function AppRoutes() {
+  const theme = useTheme();
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false, // Remove o cabeçalho que é colocado por padrão
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        }
+      }}
+    >
+      <Screen
+        name="Listagem" // Esse é o nome que irá aparecer no menu
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Screen
+        name="Cadastrar" // Esse é o nome que irá aparecer no menu
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Screen
+        name="Resumo" // Esse é o nome que irá aparecer no menu
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+    </Navigator>
+  );
+}
